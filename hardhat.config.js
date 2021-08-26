b0833564025d4a38273e28021cef4844cbfd1d88
MightTechNinja <brianwhitedev1996@outlook.com>
2021-08-26 19:31:00 +0700

    update hardhat.config.js & keys.json, env example files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 
+const loadJsonFile = require('load-json-file');
+const keys = loadJsonFile.sync("./keys.json");
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -26,8 +29,8 @@ module.exports = {
       chainId: 1337
     },
     ropsten: {
-      url: "https://ropsten.infura.io/v3/b6b6c3a1a9784f12b21b6a69a7649ebc",
-      accounts: ['0x8ceac5ba816e224a78c7662c8b7ac51b4039790806230a60e0e8d19f7a8caabe']
+      url: `https://ropsten.infura.io/v3/${keys.networks.ropsten.infuraKey}`,
+      accounts: [keys.networks.ropsten.privateKey]
     }
   },
   solidity: "0.8.4"
